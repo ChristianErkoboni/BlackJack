0b72b68a510baba8b363be1a5e19fc1286425737
Christian Erkoboni <christianerkoboni@gmail.com>
2021-04-21 22:24:39 -0400

    added a function

--- GetCard.js
+++ GetCard.js
@@ -1,9 +1,27 @@
 var suit = ["Hearts", "Spades", "Diamonds", "Clubs"];
-var number = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"];
+var number = ["2", "3", "4", "5", "6", "7", "8", "9", "10"];
 
-var randomSuit = suit[Math.floor(Math.random() * suit.length)];
+/*var randomSuit = suit[Math.floor(Math.random() * suit.length)];
 var randomNumber = number[Math.floor(Math.random() * number.length)];
-var cardNumber = Number(randomNumber);
-      function getwholeCard(){
-        document.getElementById("suit").innerHTML = cardNumber + " " + "of" + " " + randomSuit;
+var cardNumber = Number(randomNumber);*/
+
+
+      function getfirstCard(){
+        var randomSuit = suit[Math.floor(Math.random() * suit.length)];
+        var randomNumber = number[Math.floor(Math.random() * number.length)];
+        var cardNumber = Number(randomNumber);
+        document.getElementById("firstCard").innerHTML = cardNumber + " " + "of" + " " + randomSuit;
+      }
+
+       function getsecondCard(){
+        var randomSuit = suit[Math.floor(Math.random() * suit.length)];
+        var randomNumber = number[Math.floor(Math.random() * number.length)];
+        var cardNumber = Number(randomNumber);
+        document.getElementById("secondCard").innerHTML = cardNumber + " " + "of" + " " + randomSuit;
+      }
+      
+      function addCards(){
+        var totalCount;
+        totalCount = 
+          document.getElementById("firstAdd").innerHTML = totalCount;
       }
