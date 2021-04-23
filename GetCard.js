27decb8e8239eba0884fd9bb7ab5a2b5dbc86a69
Christian Erkoboni <christianerkoboni@gmail.com>
2021-04-22 22:22:27 -0400

    added more buttons

--- GetCard.js
+++ GetCard.js
@@ -4,6 +4,9 @@ var number = ["2", "3", "4", "5", "6", "7", "8", "9", "10"];
 /*var randomSuit = suit[Math.floor(Math.random() * suit.length)];
 var randomNumber = number[Math.floor(Math.random() * number.length)];
 var cardNumber = Number(randomNumber);*/
+var firstCardNumber;
+var secondCardNumber;
+
 
 
       function getfirstCard(){
@@ -11,6 +14,8 @@ var cardNumber = Number(randomNumber);*/
         var randomNumber = number[Math.floor(Math.random() * number.length)];
         var cardNumber = Number(randomNumber);
         document.getElementById("firstCard").innerHTML = cardNumber + " " + "of" + " " + randomSuit;
+        firstCardNumber = cardNumber;
+        document.getElementById("startButton").disabled = true;
       }
 
        function getsecondCard(){
@@ -18,10 +23,24 @@ var cardNumber = Number(randomNumber);*/
         var randomNumber = number[Math.floor(Math.random() * number.length)];
         var cardNumber = Number(randomNumber);
         document.getElementById("secondCard").innerHTML = cardNumber + " " + "of" + " " + randomSuit;
+        secondCardNumber = cardNumber;
       }
       
-      function addCards(){
-        var totalCount;
-        totalCount = 
+      function addCards(){ 
+        var totalCount;   
+        totalCount = firstCardNumber + secondCardNumber;
           document.getElementById("firstAdd").innerHTML = totalCount;
+      }
+      
+      function resetGame(){
+         document.getElementById("startButton").disabled = false; 
+         document.getElementById("hitButton").disabled = false;
+         document.getElementById("firstCard").innerHTML = " "
+         document.getElementById("secondCard").innerHTML = " "
+         document.getElementById("firstAdd").innerHTML = " "
+
+      }
+    
+      function stayPlayerGame(){
+          document.getElementById("hitButton").disabled = true;
       }
