de7d38f843af69761a7b98b83fda2b53aa2ae0b0
Christian Erkoboni <christianerkoboni@gmail.com>
2021-04-24 20:36:49 -0400

    added logic for getting buttons to work as well as getting the card to add to the total

--- GetCard.js
+++ GetCard.js
@@ -6,8 +6,9 @@ var randomNumber = number[Math.floor(Math.random() * number.length)];
 var cardNumber = Number(randomNumber);*/
 var firstCardNumber;
 var secondCardNumber;
-
-
+var newCard = 0;
+var startButton = document.getElementById("startButton");
+var totalCount = 0;
 
       function getfirstCard(){
         var randomSuit = suit[Math.floor(Math.random() * suit.length)];
@@ -16,6 +17,9 @@ var secondCardNumber;
         document.getElementById("firstCard").innerHTML = cardNumber + " " + "of" + " " + randomSuit;
         firstCardNumber = cardNumber;
         document.getElementById("startButton").disabled = true;
+        document.getElementById("hitButton").disabled = false;
+        document.getElementById("totalButton").disabled = false;
+        document.getElementById("stayButton").disabled = false;
       }
 
        function getsecondCard(){
@@ -26,21 +30,47 @@ var secondCardNumber;
         secondCardNumber = cardNumber;
       }
       
-      function addCards(){ 
-        var totalCount;   
-        totalCount = firstCardNumber + secondCardNumber;
-          document.getElementById("firstAdd").innerHTML = totalCount;
+      function addCards(){    
+        totalCount = firstCardNumber + secondCardNumber + newCard;
+        document.getElementById("firstAdd").innerHTML = totalCount;
+         /* totalCount += newCard;
+          document.getElementById("firstAdd").innerHTML = totalCount
+          */
+      }
+
+      function hitCard(){
+        var randomSuit = suit[Math.floor(Math.random() * suit.length)];
+        var randomNumber = number[Math.floor(Math.random() * number.length)];
+        var cardNumber = Number(randomNumber);
+        document.getElementById("thirdCard").innerHTML = cardNumber + " " + "of" + " " + randomSuit;
+        newCard = cardNumber;
+
       }
       
       function resetGame(){
          document.getElementById("startButton").disabled = false; 
-         document.getElementById("hitButton").disabled = false;
+         document.getElementById("hitButton").disabled = true;
+         document.getElementById("totalButton").disabled = true;
+         document.getElementById("stayButton").disabled = true;
          document.getElementById("firstCard").innerHTML = " "
          document.getElementById("secondCard").innerHTML = " "
+         document.getElementById("thirdCard").innerHTML = " "
          document.getElementById("firstAdd").innerHTML = " "
+         totalCount = 0;
+         newCard = 0;
 
       }
     
       function stayPlayerGame(){
           document.getElementById("hitButton").disabled = true;
       }
+
+    
+    /*if (startButton.click === false){
+        document.getElementById("hitButton").disabled = true;
+    }
+    */
+    /*function reloadGame(){
+        location.reload;
+    }
+    */
